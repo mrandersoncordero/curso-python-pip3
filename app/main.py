51304ca681508926a1f0fc390d091e69f8ed7cdd
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-04-25 18:58:16 -0400

    utilizar libreria pandas en el project app

--- app/main.py
+++ app/main.py
@@ -1,16 +1,25 @@
 import utils
 import read_csv
 import charts
+import pandas as pd
 
 def run():
-  data = read_csv.read_csv('./data.csv')
+  '''
   data = list(filter(lambda item : item['Continent'] == 'South America',data))
-
   countries = list(map(lambda x: x['Country/Territory'], data))
   percentages = list(map(lambda x: x['World Population Percentage'], data))
+  '''
+
+  df = pd.read_csv('data.csv')
+  df = df[df['Continent'] == 'Africa']
   country = input('Type Country => ')
 
+  countries = df['Country/Territory'].values
+  percentages = df['World Population Percentage']
   charts.generate_pie_chart(country, countries, percentages)
+
+  data = read_csv.read_csv('./data.csv')
+  print(country)
 
   result = utils.population_by_country(data, country)
 
